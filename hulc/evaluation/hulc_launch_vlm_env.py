6f15be5aad9a6dbc6160dfe5c3df3e9787c0780a
Robert McCarthy <robertmccarthy97@gmail.com>
2023-03-28 11:24:50 +0100

    W&B tracking

--- hulc/evaluation/hulc_launch_vlm_env.py
+++ hulc/evaluation/hulc_launch_vlm_env.py
@@ -26,39 +26,15 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize, VecMonitor, VecVideoRecorder
 from stable_baselines3.common.logger import configure
 from stable_baselines3.common.callbacks import CallbackList, EveryNTimesteps, EvalCallback
+from stable_baselines3.common.monitor import Monitor
+
 import wandb
 from wandb.integration.sb3 import WandbCallback
 
+from hulc.sb3_utils.callbacks import VideoRecorderCallback, SuccessRateCallback
 
-def get_epoch(checkpoint):
-    if "=" not in checkpoint.stem:
-        return "0"
-    checkpoint.stem.split("=")[1]
-    
-def create_logger(args, env):
-    # import pdb; pdb.set_trace()
-    now = datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
-    exp_path = f"./sb3_logs/exps/{args.exp_name}-{now}/"
-    log_path = exp_path + f"{now}"
-    # set up logger
-    new_logger = configure(log_path, ["stdout", "csv", "tensorboard"])
-    
-    callback_list = []
-    callback_list += [EvalCallback(env, eval_freq=1000)]
-    if args.track:
-        callback_list += [
-            WandbCallback(
-                gradient_save_freq=10,
-                model_save_path=None,
-                verbose=1,
-            ),]
-    
-    callback = CallbackList(callback_list)
-    
-    return new_logger, callback
 
-def main():
-    seed_everything(0, workers=True)  # type:ignore
+def get_args():
     parser = argparse.ArgumentParser(description="Evaluate a trained model on multistep sequences with language goals.")
     parser.add_argument("--dataset_path", type=str, help="Path to the dataset root directory.")
 
@@ -83,21 +59,21 @@ def main():
         type=int,
         help="Specify the number of checkpoints you want to evaluate (starting from last). Only used for calvin_agent.",
     )
-
-    parser.add_argument("--debug", action="store_true", help="Print debug info and visualize environment.")
-
     # parser.add_argument("--eval_log_dir", default=None, type=str, help="Where to log the evaluation results.")
-
+    # Relevant args
     parser.add_argument("--device", default=0, type=int, help="CUDA device")
-    
+    parser.add_argument("--debug", action="store_true", default=False, help="Print debug info and visualize environment.")
     parser.add_argument('-t', '--track', action='store_true', default=False)
-    
     parser.add_argument("--exp-name", default='temp', type=str, help="Name of experiment.")
-
+    parser.add_argument("--exp-group", default='temp', type=str, help="Name of experiment group.")
+    parser.add_argument("--vlm-rewards", action='store_true', default=False, help="Whether to use VLM rewards.")
+    
     args = parser.parse_args()
-
     assert "train_folder" in args
+    
+    return args
 
+def load_calvin_items(args):
     checkpoints = []
     if args.checkpoints is None and args.last_k_checkpoints is None and args.checkpoint is None:
         print("Evaluating model with last checkpoint.")
@@ -110,76 +86,115 @@ def main():
         checkpoints = get_all_checkpoints(Path(args.train_folder))[-args.last_k_checkpoints :]
     elif args.checkpoint is not None:
         checkpoints = [Path(args.checkpoint)]
+    assert len(checkpoints) == 1
+    checkpoint = checkpoints[0]
+    
+    epoch = get_epoch(checkpoint)
+    model, env, _ = get_default_model_and_env(
+        args.train_folder,
+        args.dataset_path,
+        checkpoint,
+        env=None,
+        device_id=args.device,
+    )
+    
+    return model, env, None
 
-    env = None
-    for checkpoint in checkpoints:
-        epoch = get_epoch(checkpoint)
-        model, env, _ = get_default_model_and_env(
-            args.train_folder,
-            args.dataset_path,
-            checkpoint,
-            env=env,
-            device_id=args.device,
-        )
-        
-        # wrap env
-        env = CalvinTaskEnv(env, single_goal=True, visualize=args.debug)
-        env = CalvinVLMEnv(env, model, use_vlm_reward=True, use_prob_reward=False, init_steps=10, use_model_actions=False)
-        env = CalvinDictObsWrapper(env, ['robot_state'])
-        
-        # TODO: solve gym compatibility issues
-        # TODO: don't use TimeLimit with VLM env!!!
-        env = CustomTimeLimit(env, max_episode_steps=50)
-        print("loaded env")
-        
-        if args.debug:
-            # params
-            use_model_actions = True
-            
-            # run env
-            done = False
+def get_epoch(checkpoint):
+    if "=" not in checkpoint.stem:
+        return "0"
+    checkpoint.stem.split("=")[1]
+    
+def test_rollout(args, env):
+    model = PPO.load("/home/robert/Research/rl_envs/hulc/sb3_logs/exps/temp-24_03_2023-15_47_22/best_model", env=env)
+    # run env
+    done, i = False, 0
+    obs = env.reset()
+    while True:
+        action = env.action_space.sample()
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, done, info = env.step(action)
+        print(f"[{i}] reward: {reward}, done: {done}") #", info.trunc: {info['TimeLimit.truncated']}")
+        if done:
+            input()
             i = 0
             obs = env.reset()
-            print("reset env")
-            while True:
-                action = env.action_space.sample()
-                obs, reward, done, info = env.step(action)
-                print(i)
-                print(f"reward: {reward}, done: {done}, info.trunc: {info['TimeLimit.truncated']}")
-                if done:
-                    input()
-                    i = 0
-                    obs = env.reset()
-                else:
-                    i += 1
-                    
         else:
-            
-            # W&B tracking
-            if args.track:
-                run = wandb.init(
-                    project='CalvinVideoRewards',
-                    group='Test',
-                    entity='robertmccarthy11',
-                    sync_tensorboard=True,
-                    name=args.exp_name,
-                    monitor_gym=False,  # auto-upload the videos of agents playing the game
-                    save_code=False,  # optional
-                )
-            
-            # Wrap the environment in a DummyVecEnv and normalize the observations
-            env = DummyVecEnv([lambda: env])
-            # env = VecVideoRecorder(env, f"videos/temp", record_video_trigger=lambda x: x % 2000 == 0, video_length=50, name_prefix=f"random-agent")
-            env = VecMonitor(env, info_keywords=("success", "is_success"))
-            env = VecNormalize(env, norm_obs=True, norm_reward=False)
-            
-            # logging
-            logger, callback = create_logger(args, env)
-            
-            # Define and train the PPO agent
-            model = PPO("MultiInputPolicy", env, verbose=1)
-            model.set_logger(logger)
-            model.learn(total_timesteps=int(1e5), callback=callback)
+            i += 1
+
+def make_vlm_env(args, env, model):
+    # wrap env
+    env = CalvinTaskEnv(env, single_goal=True, visualize=args.debug)
+    if args.vlm_rewards:
+        env = CalvinVLMEnv(env, model, use_vlm_reward=True, use_prob_reward=False, init_steps=10, use_model_actions=False)
+    else:
+        # Don't use TimeLimit with VLM env
+        env = CustomTimeLimit(env, max_episode_steps=50)
+    env = CalvinDictObsWrapper(env, ['robot_state'])
+    # TODO: solve gym compatibility issues
+    return env
+    
+def create_logger(args, env):
+    # TODO: use a better naming scheme!!
+    now = datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
+    exp_path = f"./sb3_logs/exps/{args.exp_name}-{now}/"
+    log_path = exp_path + f"{now}"
+    # set up logger
+    new_logger = configure(log_path, ["stdout", "csv", "tensorboard"])
+    
+    callback_list = []
+    callback_list += [EvalCallback(env, eval_freq=1000, best_model_save_path=exp_path)]
+    callback_list += [SuccessRateCallback(vlm_rewards=args.vlm_rewards, log_freq=1000)]
+    callback_list += [VideoRecorderCallback(env, render_freq=5000, n_eval_episodes=1, deterministic=True)]
+    if args.track:
+        callback_list += [
+            WandbCallback(
+                gradient_save_freq=10,
+                model_save_path=None,
+                verbose=1,
+            ),]
+    callback = CallbackList(callback_list)
+    
+    return new_logger, callback, exp_path
+
+def main():
+    seed_everything(0, workers=True)  # type:ignore
+    
+    args = get_args()
+    model, env, _ = load_calvin_items(args)
+    # custom env
+    env = make_vlm_env(args, env, model)
+        
+    if args.debug:
+        test_rollout(args, env)
+                
+    else:
+        # W&B tracking
+        # TODO: implement sweeps??
+        if args.track:
+            run = wandb.init(
+                entity='robertmccarthy11',
+                project='CalvinVideoRewards',
+                group=args.exp_group,
+                name=args.exp_name,
+                sync_tensorboard=True,
+                monitor_gym=False,  # auto-upload the videos of agents playing the game
+                save_code=False,  # optional
+            )
+            wandb.log({'args': vars(args)})
+        
+        # Wrap the environment in a DummyVecEnv and normalize the observations
+        env = DummyVecEnv([lambda: env])
+        env = VecMonitor(env, info_keywords=("success", "is_success",))
+        env = VecNormalize(env, norm_obs=True, norm_reward=False)
+        # logging
+        logger, callback, exp_path = create_logger(args, env)
+        
+        # Define and train the PPO agent
+        model = PPO("MultiInputPolicy", env, verbose=1)
+        model.set_logger(logger)
+        model.learn(total_timesteps=int(1e6), callback=callback)
+        model.save(exp_path + "/trained_model") # TODO
 
 
 if __name__ == "__main__":
---
+++ hulc/sb3_utils/callbacks.py
@@ -0,0 +1,123 @@
+from typing import Any, Dict
+
+import gym
+import torch as th
+import numpy as np
+
+from stable_baselines3 import A2C
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.logger import Video
+
+
+class VideoRecorderCallback(BaseCallback):
+    def __init__(self, eval_env: gym.Env, render_freq: int, n_eval_episodes: int = 1, deterministic: bool = True):
+        """
+        Records a video of an agent's trajectory traversing ``eval_env`` and logs it to TensorBoard
+
+        :param eval_env: A gym environment from which the trajectory is recorded
+        :param render_freq: Render the agent's trajectory every eval_freq call of the callback.
+        :param n_eval_episodes: Number of episodes to render
+        :param deterministic: Whether to use deterministic or stochastic policy
+        """
+        super().__init__()
+        self._eval_env = eval_env
+        self._render_freq = render_freq
+        self._n_eval_episodes = n_eval_episodes
+        self._deterministic = deterministic
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self._render_freq == 0:
+            self._record_video(deterministic=True)
+            self._record_video(deterministic=False)
+        return True
+
+    def _record_video(self, deterministic=True):
+        screens = []
+        behaviour_str = 'deterministic' if deterministic else 'stochastic'
+
+        def grab_screens(_locals: Dict[str, Any], _globals: Dict[str, Any]) -> None:
+            """
+            Renders the environment in its current state, recording the screen in the captured `screens` list
+
+            :param _locals: A dictionary containing all local variables of the callback's scope
+            :param _globals: A dictionary containing all global variables of the callback's scope
+            """
+            screen = self._eval_env.render(mode="rgb_array")
+            # PyTorch uses CxHxW vs HxWxC gym (and tensorflow) image convention
+            screens.append(screen.transpose(2, 0, 1))
+
+        evaluate_policy(
+            self.model,
+            self._eval_env,
+            callback=grab_screens,
+            n_eval_episodes=self._n_eval_episodes,
+            deterministic=self._deterministic,
+        )
+        self.logger.record(
+            f"trajectory/video_{behaviour_str}",
+            Video(th.ByteTensor(np.array([screens])), fps=40),
+            exclude=("stdout", "log", "json", "csv"),
+        )
+        
+# model = A2C("MlpPolicy", "CartPole-v1", tensorboard_log="runs/", verbose=1)
+# video_recorder = VideoRecorderCallback(gym.make("CartPole-v1"), render_freq=5000)
+# model.learn(total_timesteps=int(5e4), callback=video_recorder)
+
+
+class SuccessRateCallback(BaseCallback):
+    """
+    Custom callback for plotting additional values in tensorboard.
+    """
+
+    def __init__(self, vlm_rewards=False, verbose=0, log_freq=1000):
+        super().__init__(verbose)
+        self.vlm_rewards = vlm_rewards
+        self.log_freq = log_freq
+        
+        self.init_data()
+
+    def init_data(self):
+        self.data = {
+            "is_success": [],
+            "is_success_final": [],
+            "reward": [],
+            "tcp_z": [],
+            "random": []
+            }
+        if self.vlm_rewards:
+           self.data.update({
+               "r_similarity": [],
+               "r_prob": []
+               })
+        
+    def _on_step(self) -> bool:
+        assert len(self.locals['env'].envs) == len(self.locals["infos"])
+        
+        for i in range(len(self.locals['env'].envs)):
+            # Log success
+            self.data["is_success"] += [self.locals["infos"][i]["is_success"] * 1]
+            # reward
+            self.data["reward"] += [self.locals["infos"][i]["is_success"] * 1]
+            # Log tcp_z
+            self.data["tcp_z"] += [self.locals["infos"][i]['robot_info']['tcp_pos'][-1]]
+            # Log random
+            self.data["random"] += [np.random.rand()]
+            # Log vlm reward
+            if self.locals['dones'][i] == True:
+                assert self.locals["n_steps"] > 1
+                self.data["is_success_final"] += [self.locals["infos"][i]["is_success"] * 1]
+                if self.vlm_rewards:
+                    # r_similarity
+                    self.data["r_similarity"] += [self.locals["infos"][i]['r_similarity']]
+                    # r_prob
+                    self.data["r_prob"] += [self.locals["infos"][i]['r_probs']]
+            
+        # Dump
+        if self.num_timesteps % self.log_freq == 0:
+            for key in self.data.keys():
+                self.logger.record(f"custom_rollout/{key}", np.mean(self.data[key]))
+            self.logger.dump(self.num_timesteps)
+            self.init_data()
+            
+        return True
